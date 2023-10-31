bb811858ead0defb614dcbf17abf76d0812602da
Jihen Souissi <92984963+jihensouissi@users.noreply.github.com>
2023-10-31 02:38:32 +0100

    'jihen3'

---
+++ kaddem/src/test/java/tn/esprit/spring/kaddem/services/EquipeTest.java
@@ -0,0 +1,23 @@
+package tn.esprit.spring.kaddem.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Equipe;
+import tn.esprit.spring.kaddem.repositories.EquipeRepository;
+
+import java.util.List;
+
+@SpringBootTest
+ class EquipeTest {
+    EquipeRepository equipeRepository;
+    @Autowired
+    EquipeServiceImpl equipeService;
+    @Test
+     void testretrieveAllEquipes()
+    {
+        List<Equipe> equipeList =equipeService.retrieveAllEquipes();
+        Assertions.assertEquals(0 ,equipeList.size());
+    }
+}
