2795c3c0db4372b2dcd0c97464ef25ca0bcd457f
Jihen Souissi <92984963+jihensouissi@users.noreply.github.com>
2023-10-31 23:01:22 +0100

    'sonar'

--- kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -16,10 +16,11 @@ public class EtudiantRestController {
 	IEtudiantService etudiantService;
 	// http://localhost:8089/Kaddem/etudiant/retrieve-all-etudiants
 	@GetMapping("/retrieve-all-etudiants")
+
 	public List<Etudiant> getEtudiants() {
-		List<Etudiant> listEtudiants = etudiantService.retrieveAllEtudiants();
-		return listEtudiants;
+		return etudiantService.retrieveAllEtudiants();
 	}
+
 	// http://localhost:8089/Kaddem/etudiant/retrieve-etudiant/8
 	@GetMapping("/retrieve-etudiant/{etudiant-id}")
 	public Etudiant retrieveEtudiant(@PathVariable("etudiant-id") Integer etudiantId) {
@@ -29,9 +30,9 @@ public class EtudiantRestController {
 	// http://localhost:8089/Kaddem/etudiant/add-etudiant
 	@PostMapping("/add-etudiant")
 	public Etudiant addEtudiant(@RequestBody Etudiant e) {
-		Etudiant etudiant = etudiantService.addEtudiant(e);
-		return etudiant;
+		return etudiantService.addEtudiant(e);
 	}
+
 
 	// http://localhost:8089/Kaddem/etudiant/remove-etudiant/1
 	@DeleteMapping("/remove-etudiant/{etudiant-id}")
@@ -42,9 +43,7 @@ public class EtudiantRestController {
 	// http://localhost:8089/Kaddem/etudiant/update-etudiant
 	@PutMapping("/update-etudiant")
 	public Etudiant updateEtudiant(@RequestBody Etudiant e) {
-		Etudiant etudiant= etudiantService.updateEtudiant(e);
-
-		return etudiant;
+		return etudiantService.updateEtudiant(e);
 	}
 
 	//@PutMapping("/affecter-etudiant-departement")
@@ -56,10 +55,10 @@ public class EtudiantRestController {
     /* Ajouter un étudiant tout en lui affectant un contrat et une équipe */
     @PostMapping("/add-assign-Etudiant/{idContrat}/{idEquipe}")
     @ResponseBody
-    public Etudiant addEtudiantWithEquipeAndContract(@RequestBody Etudiant e, @PathVariable("idContrat") Integer idContrat, @PathVariable("idEquipe") Integer idEquipe) {
-        Etudiant etudiant = etudiantService.addAndAssignEtudiantToEquipeAndContract(e,idContrat,idEquipe);
-        return etudiant;
-    }
+	public Etudiant addEtudiantWithEquipeAndContract(@RequestBody Etudiant e, @PathVariable("idContrat") Integer idContrat, @PathVariable("idEquipe") Integer idEquipe) {
+		return etudiantService.addAndAssignEtudiantToEquipeAndContract(e, idContrat, idEquipe);
+	}
+
 
 	@GetMapping(value = "/getEtudiantsByDepartement/{idDepartement}")
 	public List<Etudiant> getEtudiantsParDepartement(@PathVariable("idDepartement") Integer idDepartement) {
