ff12012094e8cf1bc1e5ee8586922052712ad60a
ahmed <ahmed.toukebri@esprit.tn>
2023-11-01 22:11:37 +0100

    test

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+~
+Nkaddem/src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java,4\c\4ca3457696d43bdcee7e1aa454bf3dbf193be975
+>
+kaddem/pom.xml,4\b\4b9e327a5c949f32df7cf12b88b79bf7aadf0491
+s
+Ckaddem/src/main/java/tn/esprit/spring/kaddem/entities/Etudiant.java,3\b\3ba7df5a5f929d3174e6b6c0901b3d95e4763a40
+~
+Nkaddem/src/test/java/tn/esprit/spring/kaddem/services/EtudiantServicetest.java,f\e\fe2cd999bcbea4ec27c632479f8292b5852495dd
--- kaddem/src/test/java/tn/esprit/spring/kaddem/services/EtudiantServicetest.java
+++ kaddem/src/test/java/tn/esprit/spring/kaddem/services/EtudiantServicetest.java
@@ -1,25 +1,29 @@
 package tn.esprit.spring.kaddem.services;
-import org.testng.annotations.Test;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
-class EtudiantServicetest{
+class EtudiantServicetest {
     @Mock
-    private EtudiantRepository etudiantRepository; // Mock the repository
+    EtudiantRepository etudiantRepository; // Mock the repository
 
     @InjectMocks
-    private EtudiantServiceImpl etudiantService; // The service to be tested
+    EtudiantServiceImpl etudiantService; // The service to be tested
 
-    @Test
+    /*@Test
     void testCreateEtudiant() {
         // Arrange
         Etudiant etudiantToCreate = new Etudiant();
@@ -32,6 +36,21 @@ class EtudiantServicetest{
         assertNotNull(createdEtudiant);
         verify(etudiantRepository, times(1)).save(etudiantToCreate);
     }
+*/
+    @Test
+    void testAddEtudiant() {
+        // Create a sample Etudiant object
+        Etudiant sampleEtudiant = new Etudiant();
+        // Mock the behavior of the repository
+        when(etudiantRepository.save(sampleEtudiant)).thenReturn(sampleEtudiant);
 
+        // Call the method you want to test
+        Etudiant result = etudiantService.addEtudiant(sampleEtudiant);
 
-}
+        // Verify that the repository's save method was called
+        verify(etudiantRepository, times(1)).save(sampleEtudiant);
+
+        // Add assertions to check the result, if necessary
+        // For example, you can check if the returned object is the same as the one you passed in.
+        assertEquals(sampleEtudiant, result);
+}}
