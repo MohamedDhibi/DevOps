376fb627bb82435c562fb9677c1ffb1e3251ce0b
Eya Belguith <eya.belguith@esprit.tn>
2023-11-03 00:09:25 +0100

    baseDTO

--- kaddem/src/main/java/tn/esprit/spring/kaddem/dto/ContratDTO.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/dto/ContratDTO.java
@@ -4,75 +4,13 @@ import tn.esprit.spring.kaddem.entities.Specialite;
 
 import java.util.Date;
 
-public class ContratDTO {
-    private Integer idContrat;
-    private Date dateDebutContrat;
-    private Date dateFinContrat;
-    private Specialite specialite;
-    private Boolean archive;
-    private Integer montantContrat;
-    private Integer etudiantId; // Use an ID to reference the associated Etudiant
+public class ContratDTO extends BaseDTO {
+
+  // Use an ID to reference the associated Etudiant
 
     public ContratDTO() {
-        // This constructor is intentionally left empty. The object can be initialized using other constructors.
+        // Constructor specific to ContratDTO
     }
 
-
-    // Getters and setters for all fields
-
-    public Integer getIdContrat() {
-        return idContrat;
-    }
-
-    public void setIdContrat(Integer idContrat) {
-        this.idContrat = idContrat;
-    }
-
-    public Date getDateDebutContrat() {
-        return dateDebutContrat;
-    }
-
-    public void setDateDebutContrat(Date dateDebutContrat) {
-        this.dateDebutContrat = dateDebutContrat;
-    }
-
-    public Date getDateFinContrat() {
-        return dateFinContrat;
-    }
-
-    public void setDateFinContrat(Date dateFinContrat) {
-        this.dateFinContrat = dateFinContrat;
-    }
-
-    public Specialite getSpecialite() {
-        return specialite;
-    }
-
-    public void setSpecialite(Specialite specialite) {
-        this.specialite = specialite;
-    }
-
-    public Boolean getArchive() {
-        return archive;
-    }
-
-    public void setArchive(Boolean archive) {
-        this.archive = archive;
-    }
-
-    public Integer getMontantContrat() {
-        return montantContrat;
-    }
-
-    public void setMontantContrat(Integer montantContrat) {
-        this.montantContrat = montantContrat;
-    }
-
-    public Integer getEtudiantId() {
-        return etudiantId;
-    }
-
-    public void setEtudiantId(Integer etudiantId) {
-        this.etudiantId = etudiantId;
-    }
+    // Additional methods specific to ContratDTO
 }
