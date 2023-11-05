f17857624ef146dce7ec90b1fb96c12572bab948
mohamed <mohamed.dhibi@esprit.tn>
2023-11-05 17:47:26 +0100

    re

---
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/EtudiantRestController.java
@@ -0,0 +1,68 @@
+package tn.esprit.spring.kaddem.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.services.IEtudiantService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/etudiant")
+public class EtudiantRestController {
+	@Autowired
+	IEtudiantService etudiantService;
+	// http://localhost:8089/Kaddem/etudiant/retrieve-all-etudiants
+	@GetMapping("/retrieve-all-etudiants")
+	public List<Etudiant> getEtudiants() {
+		return etudiantService.retrieveAllEtudiants();
+	}
+	// http://localhost:8089/Kaddem/etudiant/retrieve-etudiant/8
+	@GetMapping("/retrieve-etudiant/{etudiant-id}")
+	public Etudiant retrieveEtudiant(@PathVariable("etudiant-id") Integer etudiantId) {
+		return etudiantService.retrieveEtudiant(etudiantId);
+	}
+
+	// http://localhost:8089/Kaddem/etudiant/add-etudiant
+	@PostMapping("/add-etudiant")
+	public Etudiant addEtudiant(@RequestBody Etudiant e) {
+		return etudiantService.addEtudiant(e);
+
+	}
+
+	// http://localhost:8089/Kaddem/etudiant/remove-etudiant/1
+	@DeleteMapping("/remove-etudiant/{etudiant-id}")
+	public void removeEtudiant(@PathVariable("etudiant-id") Integer etudiantId) {
+		etudiantService.removeEtudiant(etudiantId);
+	}
+
+	// http://localhost:8089/Kaddem/etudiant/update-etudiant
+	@PutMapping("/update-etudiant")
+	public Etudiant updateEtudiant(@RequestBody Etudiant e) {
+		return etudiantService.updateEtudiant(e);
+	}
+
+	//@PutMapping("/affecter-etudiant-departement")
+	@PutMapping(value="/affecter-etudiant-departement/{etudiantId}/{departementId}")
+	public void affecterEtudiantToDepartement(@PathVariable("etudiantId") Integer etudiantId, @PathVariable("departementId")Integer departementId){
+		etudiantService.assignEtudiantToDepartement(etudiantId, departementId);
+    }
+//addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe)
+    /* Ajouter un étudiant tout en lui affectant un contrat et une équipe */
+    @PostMapping("/add-assign-Etudiant/{idContrat}/{idEquipe}")
+    @ResponseBody
+    public Etudiant addEtudiantWithEquipeAndContract(@RequestBody Etudiant e, @PathVariable("idContrat") Integer idContrat, @PathVariable("idEquipe") Integer idEquipe) {
+        return etudiantService.addAndAssignEtudiantToEquipeAndContract(e,idContrat,idEquipe);
+    }
+
+	@GetMapping(value = "/getEtudiantsByDepartement/{idDepartement}")
+	public List<Etudiant> getEtudiantsParDepartement(@PathVariable("idDepartement") Integer idDepartement) {
+
+		return etudiantService.getEtudiantsByDepartement(idDepartement);
+	}
+
+}
+
+
---
+++ kaddem/src/test/java/tn/esprit/spring/kaddem/TestContratWithMockito.java
@@ -0,0 +1,85 @@
+package tn.esprit.spring.kaddem;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Contrat;
+import tn.esprit.spring.kaddem.entities.Specialite;
+import tn.esprit.spring.kaddem.repositories.ContratRepository;
+import tn.esprit.spring.kaddem.services.ContratServiceImpl;
+import tn.esprit.spring.kaddem.services.IContratService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+ class TestContratWithMockito {
+
+    @InjectMocks
+    private IContratService contratService = new ContratServiceImpl();
+
+    @Mock
+    private ContratRepository contratRepository;
+
+    @Test
+    void testRetrieveAllContrats() {
+        List<Contrat> listContrats = Arrays.asList(
+                new Contrat(1, new Date(), new Date(), Specialite.CLOUD, true, 3000),
+                new Contrat(2, new Date(), new Date(), Specialite.RESEAUX, true, 4000),
+                new Contrat(3, new Date(), new Date(), Specialite.SECURITE, true, 5000)
+        );
+        when(contratRepository.findAll()).thenReturn(listContrats);
+        List<Contrat> list = contratService.retrieveAllContrats();
+        assertEquals(3, list.size());
+        verify(contratRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testAddContratWithMockito() {
+        Contrat contrat = new Contrat(1, new Date(), new Date(), Specialite.CLOUD, true, 3000);
+        when(contratRepository.save(contrat)).thenReturn(contrat);
+        Contrat addedContrat = contratService.addContrat(contrat);
+        verify(contratRepository).save(contrat);
+        assertEquals(contrat, addedContrat);
+    }
+
+    // Similar updates for other test methods...
+
+    @Test
+    void testGetChiffreAffaireEntreDeuxDates() {
+        List<Contrat> mockContrats = Arrays.asList(
+                new Contrat(1, new Date(2023, Calendar.FEBRUARY, 1), new Date(2025, Calendar.DECEMBER, 31), Specialite.CLOUD, true, 1500),
+                new Contrat(2, new Date(2024, Calendar.FEBRUARY, 3), new Date(2025, Calendar.DECEMBER, 31), Specialite.RESEAUX, true, 2000),
+                new Contrat(3, new Date(2025, Calendar.FEBRUARY, 4), new Date(2025, Calendar.DECEMBER, 31), Specialite.SECURITE, true, 2500),
+                new Contrat(4, new Date(2026, Calendar.FEBRUARY, 5), new Date(2025, Calendar.DECEMBER, 31), Specialite.CLOUD, true, 3000)
+        );
+
+        Date startDate = new Date(2023, Calendar.FEBRUARY, 1);
+        Date endDate = new Date(2025, Calendar.DECEMBER, 31);
+
+        Logger logger = (Logger) LoggerFactory.getLogger(ContratServiceImpl.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
+
+        when(contratRepository.findAll()).thenReturn(mockContrats);
+
+         contratService.getChiffreAffaireEntreDeuxDates(startDate, endDate);
+
+        List<ILoggingEvent> logEvents = listAppender.list;
+        assertEquals(0, logEvents.size());
+    }
+}
